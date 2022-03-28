bbec2c26a3b565ebf69a650a1f40fcffdfcb3ef3
Mahmoud <m4m205@hotmail.com>
2022-03-28 21:10:56 +0200

    use asunc function for fetch

--- src/services/niveaubepalingAPI.js
+++ src/services/niveaubepalingAPI.js
@@ -16,52 +16,59 @@ export default class NiveaubepalingAPI {
     return fetchOptions;
   }
 
-  static getLists() {
-    return fetch(
+  static async getLists() {
+    const res = await fetch(
       `${this.getBaseURL()}list`,
       this.getFetchOptions("GET")
-    ).then((res) => (!res.ok ? Promise.reject(res) : res));
+    );
+    return (!res.ok ? Promise.reject(res) : res);
   }
 
-  static getItems(id) {
-    return fetch(
+  static async getItems(id) {
+    const res = await fetch(
       `${this.getBaseURL()}list/${id}`,
       this.getFetchOptions("GET")
-    ).then((res) => (!res.ok ? Promise.reject(res) : res));
+    );
+    return (!res.ok ? Promise.reject(res) : res);
   }
 
-  static addItem(id, payload) {
-    return fetch(
+  static async addItem(id, payload) {
+    const res = await fetch(
       `${this.getBaseURL()}list/${id}`,
       this.getFetchOptions("POST", payload)
-    ).then((res) => (!res.ok ? Promise.reject(res) : res));
+    );
+    return (!res.ok ? Promise.reject(res) : res);
   }
 
-  static addList(payload) {
-    return fetch(
+  static async addList(payload) {
+    const res = await fetch(
       `${this.getBaseURL()}list`,
       this.getFetchOptions("POST", payload)
-    ).then((res) => (!res.ok ? Promise.reject(res) : res));
+    );
+    return (!res.ok ? Promise.reject(res) : res);
   }
 
-  static updateItem(listId, itemId, payload) {
-    return fetch(
+  static async updateItem(listId, itemId, payload) {
+    const res = await fetch(
       `${this.getBaseURL()}list/${listId}/${itemId}`,
       this.getFetchOptions("PATCH", payload)
-    ).then((res) => (!res.ok ? Promise.reject(res) : res));
+    );
+    return (!res.ok ? Promise.reject(res) : res);
   }
 
-  static deleteItem(listId, itemId) {
-    return fetch(
+  static async deleteItem(listId, itemId) {
+    const res = await fetch(
       `${this.getBaseURL()}list/${listId}/${itemId}`,
       this.getFetchOptions("DELETE")
-    ).then((res) => (!res.ok ? Promise.reject(res) : res));
+    );
+    return (!res.ok ? Promise.reject(res) : res);
   }
 
-  static deleteList(listId) {
-    return fetch(
+  static async deleteList(listId) {
+    const res = await fetch(
       `${this.getBaseURL()}list/${listId}`,
       this.getFetchOptions("DELETE")
-    ).then((res) => (!res.ok ? Promise.reject(res) : res));
+    );
+    return (!res.ok ? Promise.reject(res) : res);
   }
 }
